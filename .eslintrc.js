ec770a2ca9ef9bf6100995c8c7434cb51a18f54b
Jiseok Oh <jiseok95@gmail.com>
2021-09-01 12:16:06 +0900

    Init: Project

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["airbnb", "airbnb/hooks", "plugin:@typescript-eslint/recommended", "prettier"],
+  plugins: ["react", "jsx-a11y", "import", "prettier", "@typescript-eslint"],
+  globals: {},
+  rules: {
+    // prettier
+    // "prettier/prettier": ["error"],
+    // TypeScript
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-object-literal-type-assertion": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    // React
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react/prop-types": ["off", {}],
+    // import
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        mjs: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    camelcase: "off",
+    "@typescript-eslint/naming-convention": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+      },
+    },
+    "import/extensions": [".js", ".ts", ".mjs", ".jsx", ".tsx"],
+  },
+};
